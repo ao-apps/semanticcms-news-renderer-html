42e44add4dbc29f9806d1451eb1a046b64ed3b51
AO Industries, Inc <support@aoindustries.com>
2022-02-27 00:48:08 -0600

    Using final instead of abstract for static utility classes
    NetBeans 12.6 is warning "Constructor is never used" when abstract, and
    this cannot be suppressed with `@SuppressWarnings("unused")`.

--- src/main/java/com/semanticcms/news/renderer/html/NewsHtmlRenderer.java
+++ src/main/java/com/semanticcms/news/renderer/html/NewsHtmlRenderer.java
@@ -1,6 +1,6 @@
 /*
  * semanticcms-news-renderer-html - SemanticCMS newsfeeds rendered as HTML in a Servlet environment.
- * Copyright (C) 2016, 2017, 2019, 2020, 2021  AO Industries, Inc.
+ * Copyright (C) 2016, 2017, 2019, 2020, 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -48,7 +48,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.lang3.NotImplementedException;
 
-public abstract class NewsHtmlRenderer {
+public final class NewsHtmlRenderer {
 
 	/** Make no instances. */
 	private NewsHtmlRenderer() {throw new AssertionError();}
